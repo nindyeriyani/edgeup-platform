43d6dc13b611ce410f14e8bc0c4edc19c0e207da
Nabillakha <alyanabila486@gmail.com>
2025-07-27 17:53:44 +0700

    feat: add StoriesPage component and update layout with Nunito Sans font

---
+++ src/app/stories/page.js
@@ -0,0 +1,66 @@
+"use client";
+
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import CareerStoriesCard from "@/components/CareerStoriesCard";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function StoriesPage() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const router = useRouter();
+
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+    router.push(`/stories?search=${query}`);
+  };
+
+  return (
+    <div className=" flex flex-col min-h-screen">
+      <Navbar active="stories"/>
+      <main className=" flex-grow bg-[#F7F8F9] pt-20">
+        <section className="py-8 md:py-16 px-4 sm:px-6 lg:px-8 bg-[#F7F8F9]">
+            <div className=" flex flex-col max-w-[1280px] w-full mx-auto items-center">
+                <h2 className="text-3xl sm:text-4xl font-bold font-['Nunito_Sans'] leading-[110%] text-center mb-4 ">
+                Career Stories
+                </h2>
+                <p className=" max-w-2xl text-base sm:text-lg md:text-xl leading-[120%] text-center font-normal font-['Nunito_Sans']  mb-8 sm:mb-12">
+                    Cerita nyata dari mereka yang udah lebih dulu meniti karier. Yuk, lihat keseharian dan insight mereka
+                </p>
+                <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center">
+                        <CareerStoriesCard
+                        title="Di Balik Dashboard: Cerita Sehari Jadi Data Analyst"
+                        name="Aurora"
+                        role="Data Analyst"
+                        company="EduStat"
+                        date="5 Juli 2025"
+                        desc="Dari buka dashboard sampai presentasi ke tim, Nabila cerita gimana kerjaan analis tuh lebih dari sekadar angka."
+                        image="/images/avatar-cardstories-person1.png"
+                        />
+                        <CareerStoriesCard
+                        title="Hidup Engineer: Seru atau Pusing?"
+                        name="Dimas"
+                        role="Software Engineer"
+                        company="FinTechID"
+                        date="8 Juli 2025"
+                        desc="Dari review PR sampai ngoding modul baru, Dimas cerita keseharian sebagai engineer di dunia fintech."
+                        image="/images/avatar-cardstories-person2.png"
+                        />
+                        <CareerStoriesCard
+                        title="Sehari Jadi UI Designer: Dari Brief Sampai Prototipe"
+                        name="Rara"
+                        role="UI Designer"
+                        company="StartUp"
+                        date="14 Juli 2025"
+                        desc="Gimana rasanya jadi UI Designer seharian? Cerita ini ngebahas mulai dari brainstorming sampai prototipe di Figma."
+                        image="/images/avatar-cardstories-person3.png"
+                        />
+                    
+                </div>
+            </div>
+        </section>
+      </main>
+      <Footer />
+    </div>
+  );
+}
--- src/components/CareerStoriesCard.jsx
+++ src/components/CareerStoriesCard.jsx
@@ -4,7 +4,7 @@ import Button from "./Button";
 
 export default function CareerCard({ title, name, role, company, date, desc, image }) {
   return (
-    <div className="w-full max-w-[339px] h-auto min-h-[306px] bg-white rounded-xl p-5 flex flex-col font-['Nunito_Sans'] shadow-md border border-[#FFF4E4]" >
+    <div className="w-full max-w-[339px] h-auto min-h-[306px] bg-white rounded-xl p-5 flex flex-col font-['Nunito_Sans'] shadow-md border border-[#FFF4E4] justify-between" >
       {/* Title */}
       <h3 className=" text-lg md:text-xl text-[#13171B] font-semibold leading-[120%] mb-3 line-clamp-2 ">{title}</h3>
 
@@ -20,22 +20,18 @@ export default function CareerCard({ title, name, role, company, date, desc, ima
       </div>
 
       {/* Description */}
-      <p className="font-normal text-sm leading-[140%] text-[#000000B2]  mb-4 line-clamp-3">{desc}</p>
+      <p className="font-normal text-sm leading-[140%] text-[#000000B2]  mb-3 line-clamp-3 min-h-[30px]">{desc}</p>
 
       {/* Footer */}
       <div className="mt-auto flex flex-col">
         <p className="font-medium text-xs text-[#303B45] mb-2">{date}</p>
-        <div className="w-full"> {/* Kontrol lebar tombol */}
+        <div className="w-full">
             <Button>
-            Baca Cerita {/* Otomatis ke tengah */}
+            Baca Cerita 
             </Button>
         </div>
+      </div>
     </div>
-      
-
-
-    </div>
-    
   );
 }
 
